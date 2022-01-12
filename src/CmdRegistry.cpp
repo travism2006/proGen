3070f4a7c7a3d76fafea51652742e98adc613003
Travis Mitchum <travism2006@gmail.com>
2022-01-12 16:04:51 -0500

    Create CmdRegistry.cpp

---
+++ src/CmdRegistry.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "CmdRegistry.h"
+
+void CmdRegistry::printCmdList()
+{
+  std::cout << "" << std::endl;
+  
+  std::cout << "\tCCG Commands\t\tDescription" << std::endl;
+  std::cout << "\t------------\t\t------------------------------------" << std::endl;
+  std::cout << "\tadd\t\tfor adding/creating new features & files" << std::endl;
+  std::cout << "\tremv\t\tfor removing features & files" << std::endl;
+  std::cout << "\tmov\t\tfor moving features & files" << std::endl;
+  
+  std::cout << "" << std::endl;
+}
