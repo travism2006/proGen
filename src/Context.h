a5b52856edb38be3d8fcdf5dc69171706cc9fb4d
Travis Mitchum <travism2006@gmail.com>
2022-02-23 10:42:02 -0500

    Update Context.h

--- src/Context.h
+++ src/Context.h
@@ -3,14 +3,24 @@
 
 #include <string>
 #include <vector>
+#include "Trie.h"
+#include "Lexer.h"
+
 class Context
 {
 public:
   std::vector<std::string> getTokenStream();
   void printCmdList();
   bool echoStatus();
+  void setEcho(bool echoVal);
   bool confirmStatus();
+  void setConfirm(bool confirmVal);
 private:
   std::string currLang;
+  bool echoBack;
+  bool confirmBack;
+  Trie allCmds;
+  void initCmds();
 };
+
 #endif
