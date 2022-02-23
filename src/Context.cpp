bbd2cf4c65c7546334a6466036a3e8cd97bada98
Travis Mitchum <travism2006@gmail.com>
2022-02-23 10:49:29 -0500

    Update Context.cpp

--- src/Context.cpp
+++ src/Context.cpp
@@ -4,14 +4,44 @@
 #include "Lexer.h"
 #include "CmdRegistry.h"
 
-std::vector<std::string> Context::printCmdList()
+void Context::initCmds()
 {
-  Lexer lx;
+  this->allCmds.insert("add");
+  this->allCmds.insert("remv");
+  this->allCmds.insert("mov");
+  this->allCmds.insert("create");
+  this->allCmds.insert("eval");
+  this->allCmds.insert("echo");
+  this->allCmds.insert("confirm");
+}
+
+std::vector<std::string> Context::getTokenStream(Lexer lx)
+{
   return lx.getTokenStream();
 }
 
-void Context::printCmdList()
+ void Context::printCmdList()
 {
   CmdRegistry cmds;
   cmds.printCmdList();
+}
+
+bool Context::echoStatus()
+{
+  return this->echoBack;
+}
+
+void Context::setEcho(bool echoVal)
+{
+  this->echoBack = echoVal;
+}
+
+bool Context::confirmStatus()
+{
+  return this->confirmBack;
+}
+
+void Context::setConfirm(bool confirmVal)
+{
+  this->confirmBack = confirmVal;
 }
